7ed6ce4ba8ba0ab013967d0a2a4115cb98bd4380
Kevin Martinez <kkevinmartinez7@gmail.com>
2022-12-29 22:19:43 -0600

    cancelled method

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -32,5 +32,8 @@ class PaymentController extends Controller
 
     public function cancelled()
     {
+        return redirect()
+            ->route('home')
+            ->withErrors('You cancelled the payment.');
     }
 }
